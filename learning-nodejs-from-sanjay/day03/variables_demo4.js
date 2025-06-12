2a2b9aea310181ce0150fd77699272949ed59b2c
= <=>
2025-06-13 05:26:08 +0530

    folder structure have changed

---
+++ learning-nodejs-from-sanjay/day03/variables_demo4.js
@@ -0,0 +1,22 @@
+// Redeclartion
+
+// The variables which are declared with "var" keyword can redeclared within the samescope
+var a = 100;
+console.log(a);
+
+var a = 1200;
+console.log(a);
+
+// The variables which are declared with "let" keyword cannot redeclared within the same scope
+let b = 200;
+console.log(b);
+
+// let b = 1300;
+// console.log(b);
+
+// The variables which are declared with "let" keyword cannot redeclared within the same scope
+const c = 500;
+console.log(c);
+
+// const c = 1500;
+// console.log(c);
